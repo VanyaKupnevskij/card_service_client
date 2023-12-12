f21336358e7162ee32b34511bc27cb1b357e1320
VanyaKupnevskij <vanakupnevskij@gmail.com>
2023-12-12 16:56:36 +0200

    delete trash

--- src/pages/Model/ModelPage.jsx
+++ src/pages/Model/ModelPage.jsx
@@ -6,7 +6,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { useHttp } from "../../hooks/http.hook";
 
 import SideNavbar from '../../components/SideNavbar';
-import { DeleteIcon, NewIcon } from '../../ui/Icon';
+import { NewIcon } from '../../ui/Icon';
 
 function ModelPage() {
   const [records, setRecords] = useState([]);
@@ -28,25 +28,8 @@ function ModelPage() {
 
     setRecords(responseData);
   }, [request])
-
-  const updateRecord = async (record) => {
-    record.date = record.date.split('T')[0];
-
-    await request({
-      url: 'records/update/' + record.id,
-      data: record,
-      method: 'put'
-    })
-  }
   
-  const deleteRecord = async (id) => {
-    await request({
-      url: 'records/' + id,
-      method: 'delete'
-    })
-  }
-  
-  const createRecord = async (newOrder) => {
+  const getCalculate = async (newOrder) => {
     await request({
       url: 'records/create',
       data: newOrder,
@@ -58,39 +41,8 @@ function ModelPage() {
     getData();
   }, [getData])
 
-  function handleInputChange(ind, record, nameValue, newValue) {
-    let tempRecords = [...records];
-    tempRecords[ind][nameValue] = newValue;
-    setRecords(tempRecords);
-
-    updateRecord(record);
-  }
-  
-  function handleDelete(record) {
-    let tempRecords = records.filter(it => it.id !== record.id);
-    setRecords(tempRecords);
-
-    deleteRecord(record.id);
-  }
-
-  async function handleCreate() {
-    let newOrder = {
-      "date": null,
-      "production_valumes": 0,
-      "unit_price": 0,
-      "unit_cost": 0,
-      "meterials_cost": 0,
-      "energy_cost": 0,
-      "equipment_cost": 0,
-      "amortization": 0,
-      "marketing": 0,
-      "labor_cost": 0,
-      "transportation_cost": 0,
-      "insurance": 0,
-      "taxation": 0
-    };
-
-    await createRecord(newOrder);
+  async function handleCalculate() {
+    await getCalculate();
     await getData();
   }
 
@@ -103,7 +55,7 @@ function ModelPage() {
           <h1 className={pageGlobalStyles.title}>Модель</h1>
           <div className={pageGlobalStyles.content_inner}>
             <button className={styles.button_new_client}
-              onClick={handleCreate}>
+              onClick={handleCalculate}>
               <NewIcon />
               <span>Обрахувати</span>
             </button>
